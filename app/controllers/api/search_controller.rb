186f672cabc9223e59f457af8e120272d2ace847
Dias Iskrayev <sparkymusic22@gmail.com>
2020-06-14 17:40:20 -0700

    fix search

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -1,6 +1,6 @@
 class Api::SearchController < ApplicationController
      def index
-        @songs = Song.where("lower(title) LIKE :search OR lower(genre) LIKE :search", {:search => "%#{params[:search]}%"})
+        @songs = Song.where("lower(title) LIKE :search OR lower(genre) LIKE :search OR lower(username) LIKE :search", {:search => "%#{params[:search].downcase}%"})
         render "api/songs/index"
     end
 end
